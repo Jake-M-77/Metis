e0794519143df1a572c1596f8f0e4c45a3337e27
Jake M <jakemanson77@icloud.com>
2026-05-17 17:10:05 +0100

    chore(person-service): extend RelationType enum and update schema migration

---
+++ services/person-service/prisma/migrations/20260517121727_add/migration.sql
@@ -0,0 +1,11 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "RelationType" ADD VALUE 'CHILD_OF';
+ALTER TYPE "RelationType" ADD VALUE 'PARENT_OF';
+ALTER TYPE "RelationType" ADD VALUE 'TESTING';
