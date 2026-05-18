32598fa9d45eec5f3ecb43a0c96a5d5ba33ad9a2
Jake M <jakemanson77@icloud.com>
2026-05-18 19:43:36 +0100

    chore: adjust TESTING relation inverse mapping for resolver tests

--- services/person-service/src/utils/relationMap.ts
+++ services/person-service/src/utils/relationMap.ts
@@ -5,7 +5,7 @@ export const inverseRelation = {
     CHILD_OF: "PARENT_OF",
     PARENT_OF: "CHILD_OF",
     UNKNOWN: "UNKNOWN",
-    TESTING: "TESTING",
+    TESTING: "TESTING_INVERSE", // Test-only inverse mapping used by automated relation resolution tests
 
     //More to be added later
 
