b8a0f2c1a26fd35548b644dcfaaf09de24f21a93
Jake M <jakemanson77@icloud.com>
2026-06-19 13:42:09 +0100

    feat(frontend): render person association data and navigation

--- frontend/src/pages/PeoplePage/Components/PeoplePageCard.tsx
+++ frontend/src/pages/PeoplePage/Components/PeoplePageCard.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import { PersonAssociation } from "../../../types/personAssociation";
 
 
-function PeoplePageCard() {
+function PeoplePageCard({ association }: { association: PersonAssociation}) {
 
+    
 
     return (<>
+
+    <Link to={`/person/${association.person.id}/overview`}>
 
         <div className="flex flex-col border ">
 
@@ -14,14 +19,16 @@ function PeoplePageCard() {
             </img>
 
             <div className="grid grid-cols-2 pb-2">
-                <p className="text-text-primary">Firstname: </p> <p className="text-text-secondary">test</p>
-                <p className="text-text-primary">Surname: </p> <p className="text-text-secondary">test</p>
-                <p className="text-text-primary">Date Of Birth: </p> <p className="text-text-secondary">test</p>
-                <p className="text-text-primary">RelationType: </p> <p className="text-text-secondary">test</p>
+                <p className="text-text-primary">Firstname: </p> <p className="text-text-secondary">{association.person.firstName}</p>
+                <p className="text-text-primary">Surname: </p> <p className="text-text-secondary">{association.person.lastName}</p>
+                <p className="text-text-primary">Date Of Birth: </p> <p className="text-text-secondary">{association.person.birthDate?.toString().split("T")[0]}</p>
+                <p className="text-text-primary">RelationType: </p> <p className="text-text-secondary">{association.relationType}</p>
 
             </div>
 
         </div>
+
+        </Link>
 
     </>)
 }
